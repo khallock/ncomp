38b62700a162608d1dbb001af04012d414c094b0
Kevin Hallock <hallock@ucar.edu>
2019-08-02 14:05:33 -0600

    Add linint2 function
    Updated "Makefile.am"s as necessary
    
    Added linint2 entry to wrapper.h
    
    Create CONTRIBUTING.md detailing steps to port an NCL function to NComp

--- src/ncomp/wrapper.h
+++ src/ncomp/wrapper.h
@@ -3,4 +3,7 @@
 
 #include <ncomp/types.h>
 
+int linint2(const ncomp_array*, const ncomp_array*, const ncomp_array*,
+            const ncomp_array*, const ncomp_array*, ncomp_array*,
+            int, int);
 #endif
