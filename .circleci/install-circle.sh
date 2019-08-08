753c8a6f09221fa7b4f81619a40d66922e84f3db
Kevin Hallock <hallock@ucar.edu>
2019-08-08 17:35:24 -0600

    Update

--- .circleci/install-circle.sh
+++ .circleci/install-circle.sh
@@ -5,7 +5,7 @@ set -eo pipefail
 
 conda config --set always_yes true --set changeps1 false --set quiet true
 conda config --add channels conda-forge
-conda env create -f ci/environment-dev-$(uname).yml --name=${ENV_NAME} --quiet
+conda env create -f .circleci/environment-dev-$(uname).yml --name=${ENV_NAME} --quiet
 conda env list
 source activate ${ENV_NAME}
 autoreconf --install
