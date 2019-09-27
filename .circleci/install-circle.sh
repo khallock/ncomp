c4a1ef81f579909706dca0b14c86b48b6a8b36d5
Mohammad (Moe) Abouali <mabouali@ucar.edu>
2019-09-27 11:21:17 -0600

    fixing blass to blas

--- .circleci/install-circle.sh
+++ .circleci/install-circle.sh
@@ -8,7 +8,7 @@ conda config --add channels conda-forge
 conda env create -f .circleci/environment-dev-$(uname).yml --name=${ENV_NAME} --quiet
 conda env list
 source activate ${ENV_NAME}
-conda install -y -c conda-forge blass
+conda install -y -c conda-forge blas
 conda install -y -c conda-forge lapack
 autoreconf --install
 ./configure --prefix=${CONDA_PREFIX}
