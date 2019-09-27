020cb78c6431749ebadab8dd4603ce7e6bb6b038
Mohammad (Moe) Abouali <mabouali@ucar.edu>
2019-09-27 13:22:52 -0600

    Getting circleci working

--- .circleci/install-circle.sh
+++ .circleci/install-circle.sh
@@ -8,8 +8,6 @@ conda config --add channels conda-forge
 conda env create -f .circleci/environment-dev-$(uname).yml --name=${ENV_NAME} --quiet
 conda env list
 source activate ${ENV_NAME}
-conda install -y -c conda-forge blas
-conda install -y -c conda-forge lapack
 autoreconf --install
 ./configure --prefix=${CONDA_PREFIX}
 make install
